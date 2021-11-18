dc2fe09d01b6858fbe2b44f34174517543c00de9
sarah <swhibley12@gmail.com>
2021-11-18 13:57:31 -0500

    added the github repo url

--- src/App.js
+++ src/App.js
@@ -2,5 +2,22 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1> Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://modest-khorana-3d01c6.netlify.app/" target="_blank">
+          Sarah Whibley
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/swhibley12/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
