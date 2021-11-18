6be20a6d3a4b048347271343ef5738b56d7c5e19
sarah <swhibley12@gmail.com>
2021-11-18 14:19:38 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,20 +4,22 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1> Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://modest-khorana-3d01c6.netlify.app/" target="_blank">
-          Sarah Whibley
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/swhibley12/react-weather-app"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1> Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://modest-khorana-3d01c6.netlify.app/" target="_blank">
+            Sarah Whibley
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/swhibley12/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
