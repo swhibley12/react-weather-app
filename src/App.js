e61ce5d83dfa05ae1e17fe33590cbfaaccb37998
sarah <swhibley12@gmail.com>
2021-11-20 15:27:47 -0500

    Styling application

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1> Weather App</h1>
+        <Weather />
         <footer>
           This project was coded by{" "}
           <a href="https://modest-khorana-3d01c6.netlify.app/" target="_blank">
