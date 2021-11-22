71dc87df049392f8fb91f34e32ce7f298cddf46a
sarah <swhibley12@gmail.com>
2021-11-21 20:45:22 -0500

    Update WeatherInfo.js

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,7 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
-import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
