db9a96cd4a616fbd62447ed866f9b625469c3fc2
sarah <swhibley12@gmail.com>
2021-11-21 21:58:48 -0500

    Update WeatherForecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -36,9 +36,8 @@ export default function WeatherForecast(props) {
                   <WeatherForecastDay data={dailyForecast} />
                 </div>
               );
-            } else {
-              return null;
             }
+            return null;
           })}
         </div>
       </div>
