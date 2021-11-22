0b09f4939fe95308a64f136024142cd2b0463d5c
sarah <swhibley12@gmail.com>
2021-11-21 22:33:50 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,13 +9,18 @@ export default function App() {
         <Weather defaultCity="New York" />
         <footer>
           This project was coded by{" "}
-          <a href="https://modest-khorana-3d01c6.netlify.app" target="_blank">
+          <a
+            href="https://modest-khorana-3d01c6.netlify.app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             Sarah Whibley
           </a>{" "}
           and is{" "}
           <a
             href="https://github.com/swhibley12/react-weather-app"
             target="_blank"
+            rel="noopener noreferrer"
           >
             open-sourced on GitHub
           </a>
