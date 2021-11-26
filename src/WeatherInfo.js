f19107451d5837910c66a11b0a8ad7998bcab6ee
sarah <swhibley12@gmail.com>
2021-11-25 20:43:03 -0500

    Revert "Updated WeatherInfo"
    This reverts commit d3eb046333e57f5bbcfff2743bbc0c3f147b95f5.

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,5 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
-import WeatherIcon from "./WeatherIcon";
 import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
@@ -16,10 +15,6 @@ export default function WeatherInfo(props) {
       <div className="row mt-3">
         <div className="col-6">
           <div className="clearfix">
-            <div className="float-left">
-              <WeatherIcon code={props.data.icon} size={52} />
-            </div>
-
             <div className="float-left">
               <WeatherTemperature celsius={props.data.temperature} />
             </div>
