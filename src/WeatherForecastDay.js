6438e3c542be65486505fb65a1e0b31a8d84c512
sarah <swhibley12@gmail.com>
2021-11-25 21:08:22 -0500

    Revert "Update WeatherForecastDay.js"
    This reverts commit 41cca14d9e71fbc7c8631f1b3df27ebd1821a9f8.

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -25,14 +25,14 @@ export default function WeatherForecastDay(props) {
     <div>
       <span className="WeatherForecast-day">{day()}</span>
       <WeatherIcon code={props.data.weather[0].icon} size={36} />
-      <span className="WeatherForecast-temperatures">
+      <div className="WeatherForecast-temperatures">
         <span className="WeatherForecast-temperature-max">
           {maxTemperature()}
         </span>
         <span className="WeatherForecast-temperature-min">
           {minTemperature()}
         </span>
-      </span>
+      </div>
     </div>
   );
 }
